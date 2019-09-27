e656cf9076c51d519e769c1ad6283cb690872a0c
Ivan Psarev <psarew.ivan@yandex.ru>
2019-09-27 22:13:24 +0300

    Добавил перемещение пина по вертикали

--- js/form.js
+++ js/form.js
@@ -20,6 +20,8 @@
   var effectSlider = document.querySelector('.img-upload__effect-level');
   var inputNoneEffect = document.querySelector('#effect-none');
 
+  var levelPin = document.querySelector('.effect-level__pin');
+
   var defaultEffectValues = {
     imgScale: '100',
     effect: 'effect-none',
@@ -150,5 +152,43 @@
       removeImgEffects();
       changeImgEffects(evt);
     }
+  });
+
+  // Перемещение ползунка глубины эффекта
+  levelPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCords = {
+      x: evt.clientX
+    };
+    var rightRange = startCords.x;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCords.x - moveEvt.clientX
+      };
+
+      startCords = {
+        x: moveEvt.clientX
+      };
+
+      console.log(rightRange + ':' + startCords.x);
+
+      if (shift.x <= rightRange) {
+        levelPin.style.left = (levelPin.offsetLeft - shift.x) + 'px';
+      }
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
